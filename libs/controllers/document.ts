7596334c20619e11ba2201fa76128dddde459aa9
Sanjay George <sanjaygeorge16@gmail.com>
2024-09-01 18:17:40 +0530

    Minor updates

--- libs/controllers/document.ts
+++ libs/controllers/document.ts
@@ -4,6 +4,12 @@ import Document from '../database/document';
 const documentDB = new Document();
 const router = express.Router();
 
+/**
+ * Gets all documents by project Id
+ * @param {string} req.query.projectId - The ID of the project to retrieve documents for
+ * @returns {Document[]} A list of all documents
+ * @throws {Error} Throws an error if the documents could not be retrieved
+ */
 router.get("/", async (req, res) => {
     try {
         const projectId = req.query.projectId as string;
@@ -16,5 +22,78 @@ router.get("/", async (req, res) => {
     }
 });
 
+
+/**
+ * Gets a document by ID
+ * @param {string} req.params.id - The ID of the document to retrieve
+ * @returns {Document} The document with the specified ID
+ * @throws {Error} Throws an error if the document could not be retrieved
+*/
+router.get("/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const document = await documentDB.get(id);
+        res.send(JSON.stringify(document));
+    }
+    catch (ex) {
+        console.error(ex);
+        res.sendStatus(500);
+    }
+});
+
+/**
+ * Inserts a new document
+ * @param {Document} req.body - The document to insert
+ * @returns {string} The ID of the inserted document
+ */
+router.post("/", async (req, res) => {
+    try {
+        const document = req.body as Document;
+        const id = await documentDB.insert(document);
+        res.send(id);
+    }
+    catch (ex) {
+        console.error(ex);
+        res.sendStatus(500);
+    }
+});
+
+/**
+ * Updates a document
+ * @param {string} req.params.id - The ID of the document to update
+ * @param {Document} req.body - The updated document
+ * @returns {boolean} True if the document was updated, false otherwise
+ * @throws {Error} Throws an error if the document could not be updated
+*/
+router.put("/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const document = req.body;
+        const result = await documentDB.update(id, document);
+        res.send(result);
+    }
+    catch (ex) {
+        console.error(ex);
+        res.sendStatus(500);
+    }
+});
+
+/**
+ * Deletes a document
+ * @param {string} req.params.id - The ID of the document to delete
+ * @returns {boolean} True if the document was deleted, false otherwise
+ * @throws {Error} Throws an error if the document could not be deleted
+ */
+router.delete("/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const result = await documentDB.delete(id);
+        res.send(result);
+    }
+    catch (ex) {
+        console.error(ex);
+        res.sendStatus(500);
+    }
+});
 
 export default router;
--- libs/database/db_client.ts
+++ libs/database/db_client.ts
@@ -3,8 +3,9 @@ import { MongoClient } from 'mongodb';
 
 dotenv.config();
 
-export const uri = `mongodb://${encodeURIComponent(process.env.MONGO_USER)}:${encodeURIComponent(process.env.MONGO_PASSWORD)}@localhost:${process.env.MONGO_PORT}/`;
+export const uri = `mongodb://${encodeURIComponent(process.env.MONGO_USER)}:${encodeURIComponent(process.env.MONGO_PASSWORD)}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/`;
 export const dbName = process.env.MONGO_DB;
+
 
 const client = new MongoClient(uri);
 export const getClient = () => client;
