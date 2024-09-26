2268902fd2e511cfbc996e1944fa5e00612bb9fc
Sanjay George <sanjaygeorge16@gmail.com>
2024-09-26 08:35:27 +0530

    Update page.tsx

--- ui/app/page.tsx
+++ ui/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
                             Document.io lets you easily capture and annotate web pages with notes and voice memos. Simplify collaboration and keep your team aligned with clear, accessible documentation.
                         </p>
 
-                        <Button as={Link} href="/documentations" className="bg-emerald-700 text-white" variant="flat" radius="sm" size="lg">
+                        <Button as={Link} href="/projects" className="bg-emerald-700 text-white" variant="flat" radius="sm" size="lg">
                             Manage Projects
                             <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
