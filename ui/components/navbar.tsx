144dd3148c75c94dc786832b58167c68468f4d2f
Sanjay George <sanjaygeorge16@gmail.com>
2024-11-27 08:32:48 +0100

    Minor UI fixes

--- ui/components/navbar.tsx
+++ ui/components/navbar.tsx
@@ -5,8 +5,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import GithubIcon from "./icons/gh_icon";
 import ButtonPrimary from "./ButtonPrimary";
-import ButtonAccent from "./ButtonAccent";
-import ButtonBlack from "./ButtonBlack";
+import RightArrowIcon from "./icons/right_arrow";
 
 
 const links = [
@@ -20,13 +19,13 @@ export default function Nav() {
         <Navbar className="border-b-1 border-b-slate-50 py-2">
             <NavbarBrand>
                 {/* <Logo /> */}
-                <Link href='/'><p className="font-bold text-slate-400 border-b-2 border-b-transparent hover:border-b-accent hover:text-slate-600 pb-1">DOCUMENT.IO</p></Link>
+                <Link href='/'><p className="font-bold text-slate-500">DOCUMENT.<span className="text-accent">IO</span></p></Link>
             </NavbarBrand>
 
             <NavbarContent className="hidden sm:flex gap-6" justify="center">
                 {links.map((link) => (
                     <NavbarItem key={link.name} isActive={link.href === pathname}>
-                        <Link className="text-slate-400 border-b-2 border-b-transparent hover:border-b-accent hover:text-slate-600 pb-1" href={link.href}>{link.name}</Link>
+                        <Link className="text-slate-400 hover:text-slate-600" href={link.href}>{link.name}</Link>
                     </NavbarItem>
                 ))}
             </NavbarContent>
@@ -36,12 +35,8 @@ export default function Nav() {
                     <a href="https://github.com/Sanjay-George/document.io" target="_blank"><GithubIcon /></a>
                 </NavbarItem>
                 <NavbarItem>
-                    <ButtonBlack
-                        href="https://github.com/Sanjay-George/document.io-chrome-extension"
-                        target="_blank"
-                        text="Download Extension"
-                        icon={null}
-                    />
+                    <Button as={Link} href="https://github.com/Sanjay-George/document.io-chrome-extension"
+                        target="_blank" variant="light" className="text-slate-500">Get Companion App <RightArrowIcon /></Button>
                 </NavbarItem>
             </NavbarContent>
         </Navbar>
