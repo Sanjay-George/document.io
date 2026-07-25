6684822b2ab736d1fdd9ddaa0e7eeba34e2108f3
Sanjay George <sanjaygeorge16@gmail.com>
2026-07-25 20:59:20 +0200

    Fix ui package config

--- ui/next.config.mjs
+++ ui/next.config.mjs
@@ -1,6 +1,12 @@
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "standalone"
+    output: "standalone",
+    outputFileTracingRoot: __dirname
 };
 
 export default nextConfig;
